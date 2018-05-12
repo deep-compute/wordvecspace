9f8b581b6503b5079d4895a54b4dc27e81f534d8
RamanjaneyuluIdavalapati <ramanjaneyulu@deepcompute.com>
2018-05-12 20:25:41 +0530

    wordvecspace update that loads normalised vectors

--- wordvecspace/base.py
+++ wordvecspace/base.py
@@ -20,74 +20,15 @@ class WordVecSpace(object):
 
     @abstractmethod
     def _perform_sgemv(self, mat, v, vec_out, nvecs, dim):
-        '''
-        cblas_sgemv is used to multiply vector and matrix.
+        res = np.dot(mat, v.T)
 
-        CblasRowMajor                   -> Multiiply in a row major order
-        CblasNoTrans                    -> Whether to transpose matix or not
-        nvecs, dim                      -> Rows, columns of Matrix
-        c_float(1.0)                    -> Scaling factor for the product of matrix and vector
-        mat.ctypes.data_as(c_void_p)    -> matrix
-        dim                             -> Columns of matirx
-        v.ctypes.data_as(c_void_p)      -> vector
-        incX                            -> Stride within X. For example, if incX is 7, every 7th element is used.
-        c_float(0.0)                    -> Scaling factor for vector.
-        vec_out.ctypes.data_as(c_void_p)-> result vector
-        incY                            -> Stride within Y. For example, if incY is 7, every 7th element is used
-
-        Read more                       -> https://developer.apple.com/documentation/accelerate/1513065-cblas_sgemv?language=objc
-        '''
-        cblas.cblas_sgemv(CblasRowMajor,
-                          CblasNoTrans,
-                          nvecs,
-                          dim,
-                          c_float(1.0),
-                          mat.ctypes.data_as(c_void_p),
-                          dim,
-                          v.ctypes.data_as(c_void_p),
-                          incX,
-                          c_float(0.0),
-                          vec_out.ctypes.data_as(c_void_p),
-                          incY)
-        return vec_out
+        return res
 
     @abstractmethod
     def _perform_sgemm(self, mat_a, mat_b, mat_out):
-        '''
-        cblas_sgemm is for multiplying matrix and matrix
+        res = np.dot(mat_a, mat_b.T)
 
-        CblasRowMajor                   -> Specifies row-major (C)
-        CblasNoTrans                    -> Specifies whether to transpose matrix mat_a
-        CblasTrans                      -> Specifies whether to transpose matrix mat_b
-        len(mat_a)                      -> Rows of result(Rows of mat_out)
-        len(mat_b)                      -> Columns of result(Columns of mat_out)
-        self.dim                        -> Common dimension in mat_a and mat_b
-        c_float(1.0)                    -> Scaling factor for the product of matrices mat_a and mat_b
-        mat_a.ctypes.data_as(c_void_p)  -> matrix mat_a
-        self.dim                        -> Columns of mat_a
-        mat_b.ctypes.data_as(c_void_p)  -> matrix mat_b
-        self.dim                        -> Columns of mat_b
-        c_float(0.0)                    -> Scaling factor for matrix mat_out
-        mat_out.ctypes.data_as(c_void_p)-> matirx mat_out
-        len(mat_b)                      -> Columns of mat_out
-
-        Read more                       -> https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc
-        '''
-        cblas.cblas_sgemm(CblasRowMajor,
-                          CblasNoTrans,
-                          CblasTrans,
-                          len(mat_a),
-                          len(mat_b),
-                          self.dim,
-                          c_float(1.0),
-                          mat_a.ctypes.data_as(c_void_p),
-                          self.dim,
-                          mat_b.ctypes.data_as(c_void_p),
-                          self.dim,
-                          c_float(0.0),
-                          mat_out.ctypes.data_as(c_void_p),
-                          len(mat_b))
-        return mat_out
+        return res
 
     @abstractmethod
     def does_word_exist(self, word):
